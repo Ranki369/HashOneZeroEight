6d13d8314abde09573e62a18f595c8858857622c
Ranki <35830596+Ranki369@users.noreply.github.com>
2023-08-17 18:52:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
+import json
 
-test_file = open("small_msg.json", "rb")
-test_url = "https://hooks.slack.com/services/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?Content-type=application/json"
-test_response = requests.post(test_url, files = {"payload": test_file})
+test_file = open("small_msg.json")
+json_data = json.load(test_file)
+headers =  {"Content-Type":"application/json"}
+print("JSON string = ", json_data)
+api_url = "https://hooks.slack.com/services/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"
+test_response = requests.post(api_url, data=json.dumps(json_data), headers=headers)
 
 if test_response.ok:
     print("Upload completed successfully!")
@@ -10,4 +14,4 @@ if test_response.ok:
 else:
     print("Something went wrong!")
   
-print("hello python")
+print("hello python - requests executed")
