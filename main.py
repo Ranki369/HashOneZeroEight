d51616d5c2d430fc5762208b84a8bbc3a50eca15
Ranki <35830596+Ranki369@users.noreply.github.com>
2023-08-17 18:22:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import requests
+
+test_file = open("small_msg.json", "rb")
+test_url = "https://hooks.slack.com/services/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?Content-type=application/json"
+test_response = requests.post(test_url, files = {"payload": test_file})
+
+if test_response.ok:
+    print("Upload completed successfully!")
+    print(test_response.text)
+else:
+    print("Something went wrong!")
+  
 print("hello python")
