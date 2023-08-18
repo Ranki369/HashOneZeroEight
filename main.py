1cf5fdd9e589a226200e1e4845919fb1852d6a18
Ranki <35830596+Ranki369@users.noreply.github.com>
2023-08-18 10:31:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-test_file = open("small_msg.json")
+test_file = open("/resources/other_layer.json")
 json_data = json.load(test_file)
 headers =  {"Content-Type":"application/json"}
 print("JSON string = ", json_data)
